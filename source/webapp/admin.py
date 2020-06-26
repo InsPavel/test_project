e1eb652c6c230066e347a61e87243d1d1a4ff80c
pavel <zhpv07@gmail.com>
2020-06-26 19:43:51 +0600

    Добавлены модели и подключены к админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Article, Category
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user_id']
+    list_filter = ['user_id']
+    search_fields = ['title', 'user_id']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='categories')
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    category_id = models.ManyToManyField('webapp.Category', related_name='articles')
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='articles')
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=3000, verbose_name='Описание')
+    image = models.ImageField(upload_to='article_image', null=True, blank=True, verbose_name='Картинка')
